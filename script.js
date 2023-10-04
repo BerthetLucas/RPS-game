c975abc473660e46eaa608ffad2c9f4e4093697f
Lucas Berthet <lu.berthet@gmail.com>
2023-10-04 20:11:08 +0200

    add win logic and try to identify the issue of choice storage

--- script.js
+++ script.js
@@ -7,34 +7,29 @@ const playTwo = document.querySelector(".j2-modale");
 
 const btnPlayerOne = document.querySelector(".btn-j1");
 const btnPlayerTwo = document.querySelector(".btn-j2");
+const gameScreen = document.querySelector(".game-modale");
+const resultScreen = document.querySelector(".result-modale");
+const btnToResult = document.querySelector(".btn-result");
 
 // Players choices DOM element
-const playOneRock = document.querySelector(".j1-rock");
-const playOnePaper = document.querySelector(".j1-paper");
-const playOneScissors = document.querySelector(".j1-scissors");
+const playerOneRock = document.querySelector(".j1-rock");
+const playerOnePaper = document.querySelector(".j1-paper");
+const playerOneScissors = document.querySelector(".j1-scissors");
 
-const playTwoRock = document.querySelector(".j2-rock");
-const playTwoPaper = document.querySelector(".j2-paper");
-const playTwoScissors = document.querySelector(".j2-scissors");
+const playerTwoRock = document.querySelector(".j2-rock");
+const playerTwoPaper = document.querySelector(".j2-paper");
+const playerTwoScissors = document.querySelector(".j2-scissors");
 
 // Welcome screen
 
 btnStartGame.addEventListener("click", toPlayerOneScreen);
-playOne.addEventListener("click",playerOneChoice); 
-
-
-function playOneChoice () {
-    
-}
-
-
 
 function toPlayerOneScreen() {
   welcomeScreen.style.display = "none";
   playOne.style.display = "block";
 }
 
-// Player one screen
+// Player one screen to player 2
 
 btnPlayerOne.addEventListener("click", toPlayerTwoScreen);
 
@@ -43,4 +38,109 @@ function toPlayerTwoScreen() {
   playTwo.style.display = "block";
 }
 
-// Player two screen
+// Player two screen to game screen
+
+btnPlayerTwo.addEventListener("click", toFightScreen);
+
+function toFightScreen() {
+  playTwo.style.display = "none";
+  gameScreen.style.display = "block";
+}
+
+// Game screen to result screen
+
+btnToResult.addEventListener("click", toResultScreen);
+
+function toResultScreen() {
+  gameScreen.style.display = "none";
+  resultScreen.style.display = "block";
+}
+
+// Game algorythm player's one choice
+
+let choicePlayerOne = "";
+
+function playerChooseRock() {
+  playerOneRock.addEventListener("click", function () {
+    choicePlayerOne = playerOneRock.value;
+    console.log(choicePlayerOne);
+    toPlayerTwoScreen();
+    return choicePlayerOne;
+  });
+}
+
+function playerChoosePaper() {
+  playerOnePaper.addEventListener("click", function () {
+    choicePlayerOne = playerOnePaper.value;
+    console.log(choicePlayerOne);
+    toPlayerTwoScreen();
+    return choicePlayerOne;
+  });
+}
+
+function playerChooseScissors() {
+  playerOneScissors.addEventListener("click", function () {
+    choicePlayerOne = playerOneScissors.value;
+    console.log(choicePlayerOne);
+    toPlayerTwoScreen();
+    return choicePlayerOne;
+  });
+}
+
+playerChoosePaper();
+playerChooseRock();
+playerChooseScissors();
+
+// Game algorythm player's two choice
+
+let choicePlayerTwo = "";
+
+function playerChooseRockTwo() {
+  playerTwoRock.addEventListener("click", function () {
+    choicePlayerTwo = playerTwoRock.value;
+    console.log(choicePlayerTwo);
+
+    toFightScreen();
+    return choicePlayerTwo;
+  });
+}
+
+function playerChoosePaperTwo() {
+  playerTwoPaper.addEventListener("click", function () {
+    choicePlayerTwo = playerTwoPaper.value;
+    console.log(choicePlayerTwo);
+    toFightScreen();
+    return choicePlayerTwo;
+  });
+}
+
+function playerChooseScissorsTwo() {
+  playerTwoScissors.addEventListener("click", function () {
+    choicePlayerTwo = playerTwoScissors.value;
+    console.log(choicePlayerTwo);
+    toFightScreen();
+    return choicePlayerTwo;
+  });
+}
+
+playerChoosePaperTwo();
+playerChooseRockTwo();
+playerChooseScissorsTwo();
+
+// Game code for result
+
+if (choicePlayerOne === choicePlayerTwo) {
+  const egality = document.createElement("h2");
+  egality.innerHTML = "None of you win !";
+  resultScreen.appendChild(egality);
+} else if (
+  (choicePlayerOne === "rock" && choicePlayerTwo == "scissors") ||
+  (choicePlayerOne === "paper" && choicePlayerTwo == "rock") ||
+  (choicePlayerOne === "scissors" && choicePlayerTwo == "paper")
+) {
+  const victory = document.createElement("h2");
+  victory.innerHTML = "Player 1 wins";
+} else {
+  const victory = document.createElement("h2");
+  victory.innerHTML = "Player 2 wins";
+}
